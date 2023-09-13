43895b31618bc5ddbbbc7b0b5cce89534da81779
Yorik van Havre <yorik@uncreated.net>
2023-09-13 15:13:09 +0200

    Added thumbnails

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 This repo contains a collection of FreeCAD files containing BIM models
 made in collaboration with [Uncreated](https://uncreated.net) and [OpeningDesign](https://openingdesign.com) over the years.
 
+
+
+![file thumbnails](contact-sheet.jpg)
+
 Eveeything is licensed under [Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)](https://creativecommons.org/licenses/by-sa/4.0/). Feel free to open, analyze and reuse, as long as you respect the terms of the license (you must cite me when reusing or sharing any of the files here, and modified files must be shared under the same license)!
---
+++ create_thumbnails.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+import sys
+import os
+import zipfile
+import subprocess
+
+
+# save thumbnails from each file
+for fcstd in [f for f in  os.listdir() if f.endswith(".FCStd")]:
+
+    # Read compressed file
+    zfile = zipfile.ZipFile(fcstd)
+    files = zfile.namelist()
+
+    # Check whether we have a FreeCAD document
+    if "Document.xml" not in files:
+        print(fcstd, "doesn't look like a FreeCAD file")
+        continue
+
+    # Read thumbnail from file or use default icon
+    image = "thumbnails/Thumbnail.png"
+    if image in files:
+        image = zfile.read(image)
+    else:
+        print(fcstd, "no thumbnail in this file")
+        continue
+
+    # Write icon to output_file
+    thumb = open(fcstd.replace(".FCStd",".png"), "wb")
+    thumb.write(image)
+    thumb.close()
+    
+# create the montage
+command = "montage -background #ffffff -fill #000000 -label '%f' -font Helvetica -pointsize 10 -define png:size=128x128 -geometry 128x128+2+2 -auto-orient -tile 6x *.png contact-sheet.jpg"
+subprocess.run(command.split(" "))
+    
+# delete intermediary files
+for png in [f for f in  os.listdir() if f.endswith(".png")]:
+    os.remove(png)
+    
+
+
+
